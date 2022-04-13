b8b69bbdaa19b8765b9db94865648a7d30c5807a
Connor Slade <connor@connorcode.com>
2022-04-13 16:10:59 -0400

    🫓 tls start

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::sync::Arc;
 use std::time::Duration;
 
 use afire::{internal::http, prelude::*};
+use lazy_static::lazy_static;
 use url::Url;
+
+lazy_static! {
+    static ref ROOT_STORE: rustls::RootCertStore = {
+        let mut root_store = rustls::RootCertStore::empty();
+        root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+            rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
+                ta.subject,
+                ta.spki,
+                ta.name_constraints,
+            )
+        }));
+        root_store
+    };
+    static ref CLIENT_CONFIG: rustls::ClientConfig = rustls::ClientConfig::builder()
+        .with_safe_defaults()
+        .with_root_certificates(ROOT_STORE.to_owned())
+        .with_no_client_auth();
+}
 
 struct App;
 
 impl Middleware for App {
     fn post(&self, req: Request, _res: Response) -> MiddleResponse {
-        let timeout = Duration::from_secs(5);
+        let timeout = Some(Duration::from_secs(5));
 
         let url = match req.path.strip_prefix("/p/") {
             Some(i) => i,
@@ -22,24 +42,12 @@ impl Middleware for App {
         //     _ => {}
         // }
 
-        // Open request socket
-        let mut stream = TcpStream::connect(url.socket_addrs(|| url.port()).unwrap()[0]).unwrap();
-        stream.set_read_timeout(Some(timeout)).unwrap();
-        stream.set_write_timeout(Some(timeout)).unwrap();
-
-        // Send Data
         let http_send = gen_request(&req, &url);
-        stream.write_all(&http_send).unwrap();
-
-        // Get response
-        let mut buff = vec![0; 1024];
-        loop {
-            match stream.read(&mut buff) {
-                Err(_) => break,
-                Ok(0) => break,
-                Ok(i) => buff.extend(vec![0; i]),
-            }
-        }
+        let mut buff = match url.scheme() {
+            "http" => request(&url, &http_send, timeout),
+            "https" => unimplemented!(),
+            _ => panic!("Unsupported URL Scheme"),
+        };
         strip_buff(&mut buff);
 
         // Parse Response
@@ -55,6 +63,54 @@ impl Middleware for App {
         MiddleResponse::Add(Response::new().status(status).bytes(body).headers(headers))
     }
 }
+
+fn request(url: &Url, send: &[u8], timeout: Option<Duration>) -> Vec<u8> {
+    // Open request socket
+    let mut stream = TcpStream::connect(url.socket_addrs(|| url.port()).unwrap()[0]).unwrap();
+    stream.set_read_timeout(timeout).unwrap();
+    stream.set_write_timeout(timeout).unwrap();
+
+    // Send Data
+    stream.write_all(&send).unwrap();
+
+    // Get response
+    let mut buff = vec![0; 1024];
+    loop {
+        match stream.read(&mut buff) {
+            Err(_) => break,
+            Ok(0) => break,
+            Ok(i) => buff.extend(vec![0; i]),
+        }
+    }
+    buff
+}
+
+// fn request_tls(url: &Url, send: &[u8], timeout: Option<Duration>) -> Vec<u8> {
+//     // Open request socket
+//     let mut stream = rustls::ClientConnection::new(
+//         Arc::new(*CLIENT_CONFIG),
+//         url.host_str().unwrap().try_into().unwrap(),
+//     )
+//     .unwrap();
+//     let mut stream = stream.writer();
+//     // url.socket_addrs(|| url.port()).unwrap()[0]
+//     // stream.set_read_timeout(timeout).unwrap();
+//     // stream.set_write_timeout(timeout).unwrap();
+//
+//     // Send Data
+//     stream.write_all(&send).unwrap();
+//
+//     // Get response
+//     let mut buff = vec![0; 1024];
+//     loop {
+//         match stream.rea(&mut buff) {
+//             Err(_) => break,
+//             Ok(0) => break,
+//             Ok(i) => buff.extend(vec![0; i]),
+//         }
+//     }
+//     buff
+// }
 
 fn gen_request(req: &Request, url: &Url) -> Vec<u8> {
     let mut headers = req
