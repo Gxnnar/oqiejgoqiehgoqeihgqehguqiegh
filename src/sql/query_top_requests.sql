61692833c1a9cb0009ebd6a584f9bb94054acf04
Connor Slade <connor@connorcode.com>
2023-09-05 09:43:33 -0400

    Update serviceworker

--- src/sql/query_top_requests.sql
+++ src/sql/query_top_requests.sql
@@ -2,6 +2,8 @@ SELECT request_origin,
     COUNT(*) counter
 FROM requests
 WHERE response_status == 200
+    AND request_origin != 'unpkg.com'
+    AND request_origin != 'upload.wikimedia.org'
     AND request_origin NOT LIKE '%cdn%'
     AND request_origin NOT LIKE '%api%'
 GROUP BY request_origin
--- web/sw.js
+++ web/sw.js
@@ -1,16 +1,18 @@
-const HOSTNAMES = ["http://localhost:8080/"];
-const ABSOLUTE_URL_REGEX = new RegExp("^(?:[a-z+]+:)?//", "i");
-
 self.addEventListener("fetch", (event) => {
-  if (HOSTNAMES.includes(event.request.referrer)) return;
-
   let url = new URL(event.request.url);
-  let realHost = event.request.referrer.split("/p/")[1] ?? url;
+  let realHost = event.request.referrer.split("/p/")[1];
+
+  if (realHost == undefined) {
+    let req = new Request(`/p/${encodeURIComponent(url)}`, event.request);
+    return fetch(req);
+  }
+
   realHost = decodeURIComponent(realHost);
   realHost = new URL(realHost).origin;
+  let path = url.pathname.split("/p/")[1] ?? url.pathname;
 
   let req = new Request(
-    `/p/${encodeURIComponent(realHost + url.pathname)}`,
+    `/p/${encodeURIComponent(`${realHost}/${path}`)}`,
     event.request
   );
   event.respondWith(fetch(req));
